e219108a24e5f2e4e12d97f0f15f923e6d6a79d1
Anmol <anmolbhutada2510@gmail.com>
2017-07-29 09:02:25 +0530

    Auth ui login made home page as the launcher added some gradle dependencies

---
+++ app/src/main/java/com/example/anurag/myfamily/HomePage.java
@@ -0,0 +1,54 @@
+package com.example.anurag.myfamily;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+
+public class HomePage extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static final int RC_SIGN_IN = 1;
+    private String mUsername;
+    Toast Welcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            onSignedOutCleanup();
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+    private void onSignedInInitialize(String username) {
+
+        mUsername = username;
+            Welcome = Toast.makeText(this, "Hi " + mUsername.toString(), Toast.LENGTH_SHORT);
+            Welcome.show();
+
+
+    }
+    private void onSignedOutCleanup(){
+
+        mUsername = null;
+
+    }
+}
