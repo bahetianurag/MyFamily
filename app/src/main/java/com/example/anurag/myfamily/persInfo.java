1b8924a34e5cec5abbc3c28cd3ed76e08ac8793a
Anurag <anuragbaheti98@gmail.com>
2017-08-04 22:53:21 +0530

    modified directory and started personal info

---
+++ app/src/main/java/com/example/anurag/myfamily/persInfo.java
@@ -0,0 +1,13 @@
+package com.example.anurag.myfamily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class persInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pers_info);
+    }
+}
