0f14459027b2361451e73b3b606abfc71930236b
Anurag <anuragbaheti98@gmail.com>
2017-09-04 00:30:22 +0530

    directory almost complete, need to improve UI

--- app/src/main/java/com/example/anurag/myfamily/FamilyInfo.java
+++ app/src/main/java/com/example/anurag/myfamily/FamilyInfo.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -27,6 +28,7 @@ public class FamilyInfo extends AppCompatActivity {
 
     private ArrayList<String> person = new ArrayList<>();
     private ListView listView;
+    TextView title;
 
     int x=0;String a[]={"Wife","Son","Daughter in law","Daughter","Grandson","Granddaughter"};
 
@@ -49,6 +51,10 @@ public class FamilyInfo extends AppCompatActivity {
         info = ind.child(name);
 
         Toast.makeText(getApplicationContext(),info.getKey(),Toast.LENGTH_SHORT).show();
+
+        title= (TextView)findViewById(R.id.title);
+        title.setText(name+" and Family");
+
 
         listView = (ListView) findViewById(R.id.listview);
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,person);
