27e38ba6d4733d8b14cfdf38d4c828839d9a61de
Anurag <anuragbaheti98@gmail.com>
2017-09-06 20:53:03 +0530

    directory almost complete, need to improve UI

--- app/src/main/java/com/example/anurag/myfamily/FamilyInfo.java
+++ app/src/main/java/com/example/anurag/myfamily/FamilyInfo.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+//TRY ADDING IMAGES
 
 public class FamilyInfo extends AppCompatActivity {
 
